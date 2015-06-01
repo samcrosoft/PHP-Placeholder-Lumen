68e2ff39cba3ac9cd4cfd4dbcc440da832ef155c
Samuel Olowofela <samuel_olowofela@yahoo.com>
2015-06-01 17:04:46 +0100

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use Samcrosoft\Placeholder\Placeholder;
+
+
+$app->get('/', function() use ($app) {
+
+    $oPlaceholder = new Placeholder();
+
+    //$oImage =  $oPlaceholder->makePlaceHolder(500,200, 'FREE ME PLEASE', '#9BCE3B', "#fff");
+    $oImage = $oPlaceholder->makePlaceholderFromURL();
+
+    ob_start();
+    imagepng($oImage);
+    imagedestroy($oImage);
+
+    $oImageContent = ob_get_contents();
+    ob_end_clean();
+
+    return response($oImageContent, 200)
+        ->header('Content-Type', "image/png");
+
+    //return $app->welcome();
+});
